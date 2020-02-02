d364638fa3d308604151a077ce096344111a033b
Param Minesh Patel <pmp94@afsconnect1.njit.edu>
2020-02-02 02:43:42 -0500

    hello

--- DBSetup/config.sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-//please don't copy my ucid
-$dbuser = "pmp94";//your ucid
-$dbpass = "Minesh$1999";//your phpMyAdmin password
-$dbhost = "sql1.njit.edu";//whichever server you login to from web.njit.edu/mysql/phpMyAdmin
-$dbdatabase = "pmp94";//your ucid
-?>
