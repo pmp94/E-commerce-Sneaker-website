9a8756390fbf2d78af46421cc9ae59825aa6eabb
Param Minesh Patel <pmp94@afsconnect1.njit.edu>
2020-03-05 17:55:35 -0500

    tables

---
+++ myProject/tables/Order_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `Order_table` (
+                            `id` int auto_increment not null,
+                            `Product_code` int(10)  not null,
+                            `Poroduct_name` varchar(60) not null,
+                            `Customers_name` varchar(60) not null,
+                            `PhoneNumber` varchar(10) not null'
+                            `amount` varchar(4) not null,
+                            `date_created` timestamp not null default current_t$
+                            `date_modified` timestamp not null default current_$
+                            PRIMARY KEY (`id`)
+                            ) CHARACTER SET utf8 COLLATE utf8_general_ci
