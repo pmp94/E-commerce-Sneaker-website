3a7eda623ad51bcc467876fe26488e184444ed72
Param Minesh Patel <pmp94@afsconnect1.njit.edu>
2020-03-05 18:03:41 -0500

    Order_table.sql

--- myProject/tables/Order_table.sql
+++ myProject/tables/Order_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `Order_table` (
                             `Poroduct_name` varchar(60) not null,
                             `Customers_name` varchar(60) not null,
                             `PhoneNumber` varchar(10) not null'
-                            `amount` varchar(4) not null,
+                            `amount` int(4) not null,
                             `date_created` timestamp not null default current_t$
                             `date_modified` timestamp not null default current_$
                             PRIMARY KEY (`id`)
