30df074ef032dd5b71595fd34c161406091730cd
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-06 19:57:56 -0400

    Update yourorder.php

--- myProject/yourorder.php
+++ myProject/yourorder.php
@@ -12,3 +12,100 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
+
+<?php
+if (isset($_GET['pid'])) {
+  $a=$_GET['pid'];
+    $sql = mysql_query("SELECT * FROM products );
+	$productCount = mysql_num_rows($sql); // count the output amount
+    if ($productCount > 0) {
+    echo 'hello';
+  }
+else{
+  echo 'no';}
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #e9e9e9;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav input[type=text] {
+  float: right;
+  padding: 6px;
+  margin-top: 8px;
+  margin-right: 16px;
+  border: none;
+  font-size: 17px;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav a, .topnav input[type=text] {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
+</style>
+</head>
+<body>
+<center><h1> Weclom to Flipcart</h1></center>
+<div class="topnav">
+  <a href="home.html">Home</a>
+  <a href="account.html">Account</a>
+  <a href="search.html">Search</a>
+  <a class="active" href="your order.html">Your Order</a>
+  
+</div>
+
+<div style="padding-left:16px">
+  <h2>Responsive Search Bar</h2>
+  <p>Navigation bar with a search box inside of it.</p>
+  <p>Resize the browser window to see the responsive effect.</p>
+  <p>For more examples on how to add a submit button and icon inside the search bar, go back to the tutorial.</p>
+</div>
+
+</body>
+</html>
+
+
