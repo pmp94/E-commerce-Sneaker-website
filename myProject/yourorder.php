b3af2092ba4041af6ca32c745e3c71f6b924564f
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-06 19:12:35 -0400

    Update yourorder.php

--- myProject/yourorder.php
+++ myProject/yourorder.php
@@ -4,14 +4,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require("config.php");
-// Create connection
-$conn = new mysqli($dbhost, $dbuser, $dbpass);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 );
 
 ?>
