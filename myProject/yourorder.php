150c8c3562fc2a1da7707320736e79bdfdc8d717
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-06 21:10:04 -0400

    Update yourorder.php

--- myProject/yourorder.php
+++ myProject/yourorder.php
@@ -8,6 +8,13 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass);
 
 ?>
 
+<?php
+if (isset($_GET['idp'])) {
+ $a=$_GET['idp'];
+    $sql = "SELECT product_name FROM Products WHERE product_name = $a ";
+       echo "$a";
+}
+?>
 
 
 
