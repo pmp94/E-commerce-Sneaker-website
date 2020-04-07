5bb976a8cfe8a5047d114aff1b1e3c85e0d44d19
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-07 02:40:21 -0400

    Update search.php

--- myProject/search.php
+++ myProject/search.php
@@ -9,7 +9,7 @@ echo "Connected successfully";
 ?>
 <?php
 $db = new PDO($connection_string, $dbuser, $dbpass);
-$stmt = $db->prepare("SELECT id from `Users3` name = joe ");
+$stmt = $db->prepare("SELECT id from `Users3` where name = joe ");
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 		
 		 echo "$id = $result['id']";
