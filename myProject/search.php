8f2b01824f2ac13bfb91dea572370e99c791389e
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-07 01:31:05 -0400

    Update search.php

--- myProject/search.php
+++ myProject/search.php
@@ -1,1 +1,9 @@
-12
+<?php
+require("config.php");
+$conn = new mysqli($dbhost, $dbuser, $dbpass);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
