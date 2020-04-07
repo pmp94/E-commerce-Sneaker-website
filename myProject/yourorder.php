476f517eb5101e3257abda6608a7626593c6e606
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-06 21:19:30 -0400

    Update yourorder.php

--- myProject/yourorder.php
+++ myProject/yourorder.php
@@ -17,7 +17,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " ;
+        echo "id: " . $row["id"] " ;
     }
 } else {
     echo "0 results";
