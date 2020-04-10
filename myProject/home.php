1c42f8d3dcabc36854b5d219f6b5ecd852525c29
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-10 18:58:11 -0400

    Update home.php

--- myProject/home.php
+++ myProject/home.php
@@ -4,6 +4,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require("config.php");
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 ?>
 <!DOCTYPE html>
 <html>
