1f6f01ee1ea263babb4bc92e0d759d716a7a2785
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-10 19:43:58 -0400

    Update yourorder.php

--- myProject/yourorder.php
+++ myProject/yourorder.php
@@ -7,296 +7,198 @@ require("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 ?>
 
-<?php
-if (isset($_GET['idp'])) {
- $a=$_GET['idp'];
- $db = new PDO($connection_string, $dbuser, $dbpass);
-$stmt = $db->prepare('SELECT id, original_name ,price from `Products` where product_name =?');
-$stmt->execute(
-array(
-"$a"
-)
-);
-while(($data = $stmt->fetch()) !== false) {
-$id= htmlspecialchars($data['id']) ;
-$name= htmlspecialchars($data['original_name']) ;  
-$price= htmlspecialchars($data['price']) ; 
-    
-    
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 1 (if user attempts to add something to the cart from the product page)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (isset($_GET['pid'])) {
+    $pid = $_GET['pid'];
+	$wasFound = false;
+	$i = 0;
+	// If the cart session variable is not set or cart array is empty
+	if (!isset($_SESSION["cart_array"]) || count($_SESSION["cart_array"]) < 1) { 
+	    // RUN IF THE CART IS EMPTY OR NOT SET
+		$_SESSION["cart_array"] = array(0 => array("item_id" => $pid, "quantity" => 1));
+	} else {
+		// RUN IF THE CART HAS AT LEAST ONE ITEM IN IT
+		foreach ($_SESSION["cart_array"] as $each_item) { 
+		      $i++;
+		      while (list($key, $value) = each($each_item)) {
+				  if ($key == "item_id" && $value == $pid) {
+					  // That item is in cart already so let's adjust its quantity using array_splice()
+					  array_splice($_SESSION["cart_array"], $i-1, 1, array(array("item_id" => $pid, "quantity" => $each_item['quantity'] + 1)));
+					  $wasFound = true;
+				  } // close if condition
+		      } // close while loop
+	       } // close foreach loop
+		   if ($wasFound == false) {
+			   array_push($_SESSION["cart_array"], array("item_id" => $pid, "quantity" => 1));
+		   }
+	}
+	header("location: cart.php"); 
+    exit();
 }
-}
-      
 ?>
-
-
-
-<!DOCTYPE html>
-<html>
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 2 (if user chooses to empty their shopping cart)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (isset($_GET['cmd']) && $_GET['cmd'] == "emptycart") {
+    unset($_SESSION["cart_array"]);
+}
+?>
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 3 (if user chooses to adjust item quantity)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (isset($_POST['item_to_adjust']) && $_POST['item_to_adjust'] != "") {
+    // execute some code
+	$item_to_adjust = $_POST['item_to_adjust'];
+	$quantity = $_POST['quantity'];
+	$quantity = preg_replace('#[^0-9]#i', '', $quantity); // filter everything but numbers
+	if ($quantity >= 100) { $quantity = 99; }
+	if ($quantity < 1) { $quantity = 1; }
+	if ($quantity == "") { $quantity = 1; }
+	$i = 0;
+	foreach ($_SESSION["cart_array"] as $each_item) { 
+		      $i++;
+		      while (list($key, $value) = each($each_item)) {
+				  if ($key == "item_id" && $value == $item_to_adjust) {
+					  // That item is in cart already so let's adjust its quantity using array_splice()
+					  array_splice($_SESSION["cart_array"], $i-1, 1, array(array("item_id" => $item_to_adjust, "quantity" => $quantity)));
+				  } // close if condition
+		      } // close while loop
+	} // close foreach loop
+}
+?>
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 4 (if user wants to remove an item from cart)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (isset($_POST['index_to_remove']) && $_POST['index_to_remove'] != "") {
+    // Access the array and run code to remove that array index
+ 	$key_to_remove = $_POST['index_to_remove'];
+	if (count($_SESSION["cart_array"]) <= 1) {
+		unset($_SESSION["cart_array"]);
+	} else {
+		unset($_SESSION["cart_array"]["$key_to_remove"]);
+		sort($_SESSION["cart_array"]);
+	}
+}
+?>
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 5  (render the cart for the user to view on the page)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$cartOutput = "";
+$cartTotal = "";
+$pp_checkout_btn = '';
+$product_id_array = '';
+if (!isset($_SESSION["cart_array"]) || count($_SESSION["cart_array"]) < 1) {
+    $cartOutput = "<h2 align='center'>Your shopping cart is empty</h2>";
+} else {
+	// Start PayPal Checkout Button
+	$pp_checkout_btn .= '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="cmd" value="_cart">
+    <input type="hidden" name="upload" value="1">
+    <input type="hidden" name="business" value="you@youremail.com">';
+	// Start the For Each loop
+	$i = 0; 
+  foreach ($_SESSION["cart_array"] as $each_item) { 
+		$item_id = $each_item['item_id'];
+  $db = new PDO($connection_string, $dbuser, $dbpass);
+  $stmt = $db->prepare('SELECT * from `Products` where id='$item_id' LIMIT 1');
+  $stmt->execute();
+   while(($data = $stmt->fetch()) !== false) {
+			$product_name = htmlspecialchars($data['original_name']) ; ;
+			$price = htmlspecialchars($data['price']);
+		}
+		$pricetotal = $price * $each_item['quantity'];
+		$cartTotal = $pricetotal + $cartTotal;
+		setlocale(LC_MONETARY, "en_US");
+        $pricetotal = money_format("%10.2n", $pricetotal);
+		// Dynamic Checkout Btn Assembly
+		$x = $i + 1;
+		$pp_checkout_btn .= '<input type="hidden" name="item_name_' . $x . '" value="' . $product_name . '">
+        <input type="hidden" name="amount_' . $x . '" value="' . $price . '">
+        <input type="hidden" name="quantity_' . $x . '" value="' . $each_item['quantity'] . '">  ';
+		// Create the product array variable
+		$product_id_array .= "$item_id-".$each_item['quantity'].","; 
+		// Dynamic table row assembly
+		$cartOutput .= "<tr>";
+		$cartOutput .= '<td><a href="product.php?id=' . $item_id . '">' . $product_name . '</a><br /><img src="inventory_images/' . $item_id . '.jpg" alt="' . $product_name. '" width="40" height="52" border="1" /></td>';
+		$cartOutput .= '<td>$' . $price . '</td>';
+		$cartOutput .= '<td><form action="cart.php" method="post">
+		<input name="quantity" type="text" value="' . $each_item['quantity'] . '" size="1" maxlength="2" />
+		<input name="adjustBtn' . $item_id . '" type="submit" value="change" />
+		<input name="item_to_adjust" type="hidden" value="' . $item_id . '" />
+		</form></td>';
+		//$cartOutput .= '<td>' . $each_item['quantity'] . '</td>';
+		$cartOutput .= '<td>' . $pricetotal . '</td>';
+		$cartOutput .= '<td><form action="cart.php" method="post"><input name="deleteBtn' . $item_id . '" type="submit" value="X" /><input name="index_to_remove" type="hidden" value="' . $i . '" /></form></td>';
+		$cartOutput .= '</tr>';
+		$i++; 
+    } 
+	setlocale(LC_MONETARY, "en_US");
+    $cartTotal = money_format("%10.2n", $cartTotal);
+	$cartTotal = "<div style='font-size:18px; margin-top:12px;' align='right'>Cart Total : ".$cartTotal." USD</div>";
+    // Finish the Paypal Checkout Btn
+	$pp_checkout_btn .= '<input type="hidden" name="custom" value="' . $product_id_array . '">
+	<input type="hidden" name="notify_url" value="https://www.yoursite.com/storescripts/my_ipn.php">
+	<input type="hidden" name="return" value="https://www.yoursite.com/checkout_complete.php">
+	<input type="hidden" name="rm" value="2">
+	<input type="hidden" name="cbt" value="Return to The Store">
+	<input type="hidden" name="cancel_return" value="https://www.yoursite.com/paypal_cancel.php">
+	<input type="hidden" name="lc" value="US">
+	<input type="hidden" name="currency_code" value="USD">
+	<input type="image" src="http://www.paypal.com/en_US/i/btn/x-click-but01.gif" name="submit" alt="Make payments with PayPal - its fast, free and secure!">
+	</form>';
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {box-sizing: border-box;}
-
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.topnav {
-  overflow: hidden;
-  background-color: #e9e9e9;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #2196F3;
-  color: white;
-}
-
-.topnav input[type=text] {
-  float: right;
-  padding: 6px;
-  margin-top: 8px;
-  margin-right: 16px;
-  border: none;
-  font-size: 17px;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav a, .topnav input[type=text] {
-    float: none;
-    display: block;
-    text-align: left;
-    width: 100%;
-    margin: 0;
-    padding: 14px;
-  }
-  
-  .topnav input[type=text] {
-    border: 1px solid #ccc;  
-  }
-}
-</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Your Cart</title>
+<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
 </head>
 <body>
-<center><h1> Weclom to Flipcart</h1></center>
-<div class="topnav">
-  <a href="home.php">Home</a>
-  <a href="account.php">Account</a>
-  <a href="search.php">Search</a>
-  <a class="active" href="yourorder.php">Your Order</a>
-  
-</div>
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-body {
-  font-family: Arial;
-  font-size: 17px;
-  padding: 8px;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-.row {
-  display: -ms-flexbox; /* IE10 */
-  display: flex;
-  -ms-flex-wrap: wrap; /* IE10 */
-  flex-wrap: wrap;
-  margin: 0 -16px;
-}
-
-.col-25 {
-  -ms-flex: 25%; /* IE10 */
-  flex: 25%;
-}
-
-.col-50 {
-  -ms-flex: 50%; /* IE10 */
-  flex: 50%;
-}
-
-.col-75 {
-  -ms-flex: 75%; /* IE10 */
-  flex: 75%;
-}
-
-.col-25,
-.col-50,
-.col-75 {
-  padding: 0 16px;
-}
-
-.container {
-  background-color: #f2f2f2;
-  padding: 5px 20px 15px 20px;
-  border: 1px solid lightgrey;
-  border-radius: 3px;
-}
-
-input[type=text] {
-  width: 100%;
-  margin-bottom: 20px;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-
-label {
-  margin-bottom: 10px;
-  display: block;
-}
-
-.icon-container {
-  margin-bottom: 20px;
-  padding: 7px 0;
-  font-size: 24px;
-}
-
-.btn {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px;
-  margin: 10px 0;
-  border: none;
-  width: 100%;
-  border-radius: 3px;
-  cursor: pointer;
-  font-size: 17px;
-}
-
-.btn:hover {
-  background-color: #45a049;
-}
-
-a {
-  color: #2196F3;
-}
-
-hr {
-  border: 1px solid lightgrey;
-}
-
-span.price {
-  float: right;
-  color: grey;
-}
-
-/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (also change the direction - make the "cart" column go on top) */
-@media (max-width: 800px) {
-  .row {
-    flex-direction: column-reverse;
-  }
-  .col-25 {
-    margin-bottom: 20px;
-  }
-}
-</style>
-</head>
-<body>
-
-<h2>Responsive Checkout Form</h2>
-<p>Resize the browser window to see the effect. When the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other.</p>
-<div class="row">
-  <div class="col-75">
-    <div class="container">
-      <form action="/action_page.php">
-      
-        <div class="row">
-          <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com">
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="New York">
-
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="NY">
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001">
-              </div>
-            </div>
-          </div>
-
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
-            </div>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname" placeholder="John More Doe">
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-            <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September">
-            <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352">
-              </div>
-            </div>
-          </div>
-          
-        </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
-        </label>
-        <input type="submit" value="Continue to checkout" class="btn">
-      </form>
+<div align="center" id="mainWrapper">
+  <?php include_once("template_header.php");?>
+  <div id="pageContent">
+    <div style="margin:24px; text-align:left;">
+	
+    <br />
+    <table width="100%" border="1" cellspacing="0" cellpadding="6">
+      <tr>
+        <td width="18%" bgcolor="#C5DFFA"><strong>Product</strong></td>
+        <td width="45%" bgcolor="#C5DFFA"><strong>Product Description</strong></td>
+        <td width="10%" bgcolor="#C5DFFA"><strong>Unit Price</strong></td>
+        <td width="9%" bgcolor="#C5DFFA"><strong>Quantity</strong></td>
+        <td width="9%" bgcolor="#C5DFFA"><strong>Total</strong></td>
+        <td width="9%" bgcolor="#C5DFFA"><strong>Remove</strong></td>
+      </tr>
+     <?php echo $cartOutput; ?>
+     <!-- <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr> -->
+    </table>
+    <?php echo $cartTotal; ?>
+    <br />
+<br />
+<?php //echo $pp_checkout_btn; ?>
+    <br />
+    <br />
+    <a href="cart.php?cmd=emptycart">Click Here to Empty Your Shopping Cart</a>
     </div>
+   <br />
   </div>
-  <div class="col-25">
-    <div class="container">
-      <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> </span></h4>
-      <p><a><?php echo "$name"?></a> <span class="price"><?php echo "$price"?></span></p>
-      
-      
-      <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
-    </div>
-  </div>
+  <?php include_once("template_footer.php");?>
 </div>
-
 </body>
 </html>
-
-
-</body>
-</html>
-
-
