3c43d9e3c677be1543b75f026556ed39e1cf5c56
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-10 22:25:40 -0400

    Update home.php

--- myProject/home.php
+++ myProject/home.php
@@ -153,7 +153,7 @@ body {
   <div class="column">
         <div class="card">
           <form action="yourorder.php?pid=1"  method="POST">
-            <img src="images/nike1.jpeg" style="width:300px;height:250px"  >
+            <img src="images/nike.jpeg" style="width:300px;height:250px"  >
             <h1 id="1">Nike air 50</h1>
             <p class="price">$49.99</p>
             <p><button name="1">Add to Cart</button></p>
