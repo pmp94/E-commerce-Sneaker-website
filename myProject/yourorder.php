18d0d86cfb0106752064ff300fdb95baaf45a3b6
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-10 20:47:13 -0400

    Update yourorder.php

--- myProject/yourorder.php
+++ myProject/yourorder.php
@@ -158,8 +158,8 @@ $stmt = $db->prepare("SELECT * from `Products` where id='$item_id' LIMIT 1");
 	</form>';
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Your Cart</title>
