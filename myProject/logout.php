320ded947097e5b868190d30ccfe75e274aa1c01
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-14 22:23:29 -0400

    Update logout.php

--- myProject/logout.php
+++ myProject/logout.php
@@ -1,93 +1,14 @@
-
 <?php
 session_start();
 session_unset();
 session_destroy();
-
+echo "You have been logged out";
 echo var_export($_SESSION, true);
 if (ini_get("session.use_cookies")) { 
     $params = session_get_cookie_params(); 
     setcookie(session_name(), '', time() - 42000, 
         $params["path"], $params["domain"], 
         $params["secure"], $params["httponly"] 
-        header("Location: https://web.njit.edu/~pmp94/IT202/myProject/login.php");
     ); 
 } 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-* {box-sizing: border-box;}
-
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.topnav {
-  overflow: hidden;
-  background-color: #e9e9e9;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #2196F3;
-  color: white;
-}
-
-.topnav input[type=text] {
-  float: right;
-  padding: 6px;
-  margin-top: 8px;
-  margin-right: 16px;
-  border: none;
-  font-size: 17px;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav a, .topnav input[type=text] {
-    float: none;
-    display: block;
-    text-align: left;
-    width: 100%;
-    margin: 0;
-    padding: 14px;
-  }
-  
-  .topnav input[type=text] {
-    border: 1px solid #ccc;  
-  }
-}
-</style>
-</head>
-<body>
-<center><h1> Weclom to Flipcart</h1></center>
-<div class="topnav">
-  <a href="home.php">Home</a>
-  <a href="account.php">Account</a>
-  <a href="search.php">Search</a>
-  <a href="yourorder.php">Your Order</a>
-  <a href="orderhistory.php">Order History</a>
-  <a class="active" href="logout.php">Logout</a>
-  
-</div>
-</body>
-</html>
-
