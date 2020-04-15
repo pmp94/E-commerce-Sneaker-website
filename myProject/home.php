86630871c35ff163a34e98e8d3ab7bf910be3925
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-14 22:41:44 -0400

    Update home.php

--- myProject/home.php
+++ myProject/home.php
@@ -1,10 +1,11 @@
 <?php
+session_start();
 if (!isset($_SESSION['user'])) {
     echo "hello"; 
 }
 ?>
 <?php
-session_start();
+
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
