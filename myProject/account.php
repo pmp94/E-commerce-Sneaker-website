d1b806bcc0419bd746015c7b005f69210be18614
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-17 19:47:56 -0400

    Update account.php

--- myProject/account.php
+++ myProject/account.php
@@ -17,7 +17,7 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 ?>
 <?php 
       $db = new PDO($connection_string, $dbuser, $dbpass);
-      $stmt = $db->prepare("SELECT * from `Products` where id="$id" LIMIT 1");
+      $stmt = $db->prepare("SELECT * from `Users3` where id="$id" LIMIT 1");
       $stmt->execute();
       while(($data = $stmt->fetch()) !== false) {
                 $product_name = htmlspecialchars($data['original_name']) ;  
