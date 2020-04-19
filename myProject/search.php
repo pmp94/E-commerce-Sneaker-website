26aea3de98e0ed3769c58b6b7a9b69efd44cab16
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-18 22:21:37 -0400

    Update search.php

--- myProject/search.php
+++ myProject/search.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+if (!isset($_SESSION['id'])) {
+ header("location:login.php"); 
+    exit();
+}
+?>
+<?php
+session_start();
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
