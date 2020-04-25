1b0e7df6acc72e6d903978dd3de9ed095993ae90
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-04-24 21:49:31 -0400

    Update yourorder.php

--- myProject/yourorder.php
+++ myProject/yourorder.php
@@ -15,6 +15,29 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 ?>
 
 <?php 
+if (isset($_POST['confirm'])) {
+  if(count($_SESSION["cart_array"]) > 0){
+  $i = 0; 
+    foreach ($_SESSION["cart_array"] as $each_item) { 
+    $item_id = $each_item['item_id'];
+      $db = new PDO($connection_string, $dbuser, $dbpass);
+      $stmt = $db->prepare("SELECT * from `Products` where id='$item_id' LIMIT 1");
+      $stmt->execute();
+      while(($data = $stmt->fetch()) !== false) {
+                $product_name = htmlspecialchars($data['original_name']) ;  
+                $price= htmlspecialchars($data['price']) ; 
+                $img = htmlspecialchars($data['product_name']) ;
+     
+}
+  
+
+    } 
+}
+}
+
+?>
+
+<?php 
 if (isset($_GET['pid'])) {
   $pid = $_GET['pid'];
   $wasFound = false;
