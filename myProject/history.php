14d5f623ef1996f88cdce43e1231cbeffb4a9f27
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-05-02 18:48:36 -0400

    Update history.php

--- myProject/history.php
+++ myProject/history.php
@@ -14,7 +14,7 @@ require("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 ?>
 <?php 
-if(isset($_GET["history"])){
+
 echo '2';
    $db = new PDO($connection_string, $dbuser, $dbpass);
       $stmt = $db->prepare("SELECT * from `history` where User_id='" . $_SESSION['id'] . "'");
@@ -24,7 +24,7 @@ echo '2';
        while(($data = $stmt->fetch()) !== false) {
         echo '1';
                 
-}
+
 }
 ?>
 <!DOCTYPE html>
