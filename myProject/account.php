c997f37db82824ce79dbc90ab1a1feb23b2ff21f
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-05-02 20:36:49 -0400

    Update account.php

--- myProject/account.php
+++ myProject/account.php
@@ -75,7 +75,12 @@ if(isset($_POST["updatepassword"])){
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <style>
 * {box-sizing: border-box;}
 
@@ -142,16 +147,6 @@ body {
   <a href="yourorder.php">Your Order</a>
   <a href="history.php">Order History</a>
 </div>
-</body>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
 <div class="row">
 	<div class="col-sm-6" style="background-color:lavender;">
 		<div class="container py-2">
