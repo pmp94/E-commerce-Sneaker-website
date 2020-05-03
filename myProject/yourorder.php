05b19085efa7d1ed8e083ae313a681e485322efe
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-05-02 21:41:30 -0400

    Update yourorder.php

--- myProject/yourorder.php
+++ myProject/yourorder.php
@@ -130,9 +130,9 @@ if (!isset($_SESSION["cart_array"]) || count($_SESSION["cart_array"]) < 1) {
 ?>
 <?php 
 if (isset($_GET['done']) && $_GET['done'] == "confirm") {
- $h = "<script> 
-       document.getElementById("size"); 
-      </script>";
+ $h = '<script type="text/javascript">' . 
+      'document.getElementById("size").innerHTML;' .
+      '</script>';
   echo "$h";
 }
 ?>
