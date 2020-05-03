11bb39de51fc0c876e2a359a7d463ebe85439d1d
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-05-02 20:45:56 -0400

    Update account.php

--- myProject/account.php
+++ myProject/account.php
@@ -75,14 +75,7 @@ if(isset($_POST["updatepassword"])){
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</head>
-<head>	
+
 <style>
 * {box-sizing: border-box;}
 
@@ -123,6 +116,19 @@ body {
   margin-right: 16px;
   border: none;
   font-size: 17px;
+}
+
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
 }
 
 @media screen and (max-width: 600px) {
