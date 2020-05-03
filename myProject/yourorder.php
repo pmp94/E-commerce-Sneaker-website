01e7b626d8b6c371d2d5b9da768166a594196dc7
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-05-02 21:51:53 -0400

    Update yourorder.php

--- myProject/yourorder.php
+++ myProject/yourorder.php
@@ -131,7 +131,7 @@ if (!isset($_SESSION["cart_array"]) || count($_SESSION["cart_array"]) < 1) {
 <?php 
 if (isset($_GET['done']) && $_GET['done'] == "confirm") {
  $h = "<script type='text/javascript'>
-                document.getElementById("size").value;
+                document.getElementById('size').value;
             </script>";
   echo "$h";
 }
