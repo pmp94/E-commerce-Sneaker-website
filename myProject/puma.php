df56b5b88bc5444a4f1d7c9bea0fddd09b7ec8d7
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-05-03 21:55:03 -0400

    Update puma.php

--- myProject/puma.php
+++ myProject/puma.php
@@ -1,1 +1,146 @@
-1
+<?php
+session_start();
+if (!isset($_SESSION['id'])) {
+ header("location:login.php"); 
+    exit();
+}
+?>
+<?php
+session_start();
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require("config.php");
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #e9e9e9;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav input[type=text] {
+  float: right;
+  padding: 6px;
+  margin-top: 8px;
+  margin-right: 16px;
+  border: none;
+  font-size: 17px;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav a, .topnav input[type=text] {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
+</style>
+</head>
+<body>
+<div class="topnav">
+  <a href="home.php">Home</a>
+  <a href="account.php">Account</a>
+  <a class="active" href="search.php">Search</a>
+  <a href="yourorder.php">Your Order</a>
+  <a href="history.php">Order History</a>
+</div>
+<div class="row">
+  <div class="column">
+        <div class="card">
+          <form action="yourorder.php?pid=10"  method="POST">
+            <img src="images/puma1.jpeg" style="width:300px;height:250px"  >
+            <h1 id="1">Puma Cali</h1>
+            <p class="price">$49.99</p>
+            <p><button name="1">Add to Cart</button></p>
+            </form>
+        </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <form action="yourorder.php?pid=11"  method="POST">
+      <img src="images/puma2.jpeg" style="width:300px;height:250px" >
+      <h1 id="2">Puma RX-S</h1>
+      <p class="price">$99.99</p>
+      <p><button name="2">Add to Cart</button></p>
+      </form>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <form action="yourorder.php?pid=12" method="POST">
+      <img src="images/puma3.jpeg" style="width:300px;height:250px" style="width:100%">
+      <h1 id="6">Puma Future Rider</h1><?php?>
+      <p class="price">$149.99</p>
+      <p><button name="6">Add to Cart</button></p>
+      </form>
+    </div>
+  </div>
+</div>
+<div style="padding-left:16px">
+  <h2>You may also like</h2>
+</div>
+
+<div class="row">
+  <div class="column">
+        <div class="card">
+          <form action="yourorder.php?pid=13" method="POST">
+            <img src="images/puma4.jpeg" style="width:300px;height:250px" style="width:100%">
+            <h1 id="3">Puma GV Hacked</h1>
+            <p class="price">$199.99</p>
+            <p><button name="3">Add to Cart</button></p>
+            </form>
+        </div>
+  </div>
+ <div class="column">
+    <div class="card">
+      <form action="yourorder.php?pid=14" method="POST">
+      <img src="images/puma5.jpeg" style="width:300px;height:250px" style="width:100%">
+      <h1 id="5">Puma Rise</h1>
+      <p class="price">$249.99</p>
+      <p><button name="4">Add to Cart</button></p>
+      </form>
+    </div>
+  </div>
+
+</body>
+</html>
