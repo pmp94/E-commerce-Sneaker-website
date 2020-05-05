1b9b51db3c574279f0cc1bdcbc7a45d4ca6a3315
pmp94 <60564460+pmp94@users.noreply.github.com>
2020-05-05 18:41:33 -0400

    Update admin_sell.php

--- myProject/admin_sell.php
+++ myProject/admin_sell.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['id'])) {
- header("location:admin_login.php"); 
+ header("location:login.php"); 
     exit();
 }
 ?>
@@ -17,7 +17,7 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $cartOutput = "";
 $datas = array();
 $db = new PDO($connection_string, $dbuser, $dbpass);
-      $stmt = $db->prepare("SELECT * from `admin`);
+      $stmt = $db->prepare("SELECT * from `admin`");
       $stmt->execute();
        while(($data = $stmt->fetch()) !== false) {
              $datas[] = $data;        
@@ -25,7 +25,7 @@ $db = new PDO($connection_string, $dbuser, $dbpass);
 $i=0;
 foreach($datas as $data){ 
 $cartOutput .= "<tr>";
-$cartOutput .= '<td><a>' .$data['Product_name'] . '</a></td>';
+$cartOutput .= '<td>' . $data['Product_name'] . '</td>';
 $cartOutput .= '<td>' . $data['Quantity'] . '</td>';
 $cartOutput .= '</tr>';
 $i++ ;
@@ -35,9 +35,69 @@ $i++ ;
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #e9e9e9;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav input[type=text] {
+  float: right;
+  padding: 6px;
+  margin-top: 8px;
+  margin-right: 16px;
+  border: none;
+  font-size: 17px;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav a, .topnav input[type=text] {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
+</style>
 </head>
 <body>
-<div style="padding-left:16px">
+<div class="topnav">
+  <a class="active" href="admin_sell.php">Sell History</a>
+</div>
+ <div style="padding-left:16px">
 <div align="center" id="mainWrapper">
   
   <div id="pageContent">
